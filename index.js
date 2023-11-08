1968bbbe379c7e57c2bf16727dfc721e6901adef
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-11-08 14:25:42 +0100

    creazione array obj films/series

---
+++ index.js
@@ -0,0 +1,24 @@
+const arrayFilms = [
+    {
+        title: "Il padrino",
+        year: 1972,
+        genre: "drama",
+        rating: 9.2,
+        type: "movie",
+      },
+      {
+        title: "Il Signore degli Anelli: La compagnia dell'anello",
+        year: 2001,
+        genre: "fantasy",
+        rating: 9.0,
+        type: "movie",
+      },
+      {
+        title: "Breaking Bad",
+        year: 2008,
+        genre: "crime",
+        rating: 9.5,
+        type: "tv",
+        seasons: 5,
+      },
+]
