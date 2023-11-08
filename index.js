1a8dc67aeb7171eff9f48264e9eb76ac9d0b8497
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-11-08 16:22:42 +0100

    function mediaRating()

--- index.js
+++ index.js
@@ -1,27 +1,87 @@
 const arrayFilms = [
     {
-        title: "Il padrino",
+        title: "Il Padrino",
         year: 1972,
         genre: "drama",
         rating: 9.2,
-        type: "movie",
+        type: "movie"
       },
       {
         title: "Il Signore degli Anelli: La compagnia dell'anello",
         year: 2001,
         genre: "fantasy",
         rating: 9.0,
-        type: "movie",
+        type: "movie"
+      },
+      {
+        title: "Pulp Fiction",
+        year: 1994,
+        genre: "crime",
+        rating: 9.0,
+        type: "movie"
+      },
+      {
+        title: "Shining",
+        year: 1980,
+        genre: "crime",
+        rating: 8.5,
+        type: "movie"
+      },
+      {
+        title: "La ricerca della felicità",
+        year: 2006,
+        genre: "drama",
+        rating: 8.2,
+        type: "movie"
+      },
+      {
+        title: "Parasite",
+        year: 2019,
+        genre: "drama",
+        rating: 9.1,
+        type: "movie"
       },
       {
         title: "Breaking Bad",
         year: 2008,
         genre: "crime",
         rating: 9.5,
-        type: "series",
-        seasons: 5,
+        type: "tv",
+        seasons: 9
       },
-]
+      {
+        title: "The Wire",
+        year: 2002,
+        genre: "crime",
+        rating: 9.2,
+        type: "tv",
+        seasons: 5
+      },
+      {
+        title: "Game of Thrones",
+        year: 2011,
+        genre: "fantasy",
+        rating: 9.3,
+        type: "tv",
+        seasons: 10
+      },
+      {
+        title: "The Sopranos",
+        year: 1999,
+        genre: "crime",
+        rating: 9.2,
+        type: "tv",
+        seasons: 3
+      },
+      {
+        title: "Mad Men",
+        year: 2007,
+        genre: "drama",
+        rating: 9.2,
+        type: "tv",
+        seasons: 4
+      },
+    ];
 
 //Creare una classe Movie che contenga le informazioni sopra indicate.
 class Movie {
@@ -41,13 +101,13 @@ class Movie {
 
 //Creare una classe TvSeries che estenda la classe Movie e ne aggiunta la proprietà seasons.
 class TvSerie extends Movie{
-    constructor(title, year, genre, rating, type, season){
+    constructor(title, year, genre, rating, type, seasons){
         super(title, year, genre, rating, type);
-        this.season = season;
+        this.season = seasons;
     }
 
     toString(){
-        return `${this.title} è un ${this.type}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.season} stagioni. Ha un voto di ${this.rating}`
+        return `${this.title} è una serie ${this.type}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.season} stagioni. Ha un voto di ${this.rating}`
     }
 }
 
@@ -56,12 +116,26 @@ class TvSerie extends Movie{
 const filterType = arrayFilms.map(x=> {
     if(x.type === 'movie'){
         const film = new Movie(x.title, x.year, x.genre, x.rating, x.type);
-        console.log(film);
-    } else if (x.type === 'series'){
-        const serieTV = new TvSerie(x.title, x.year, x.genre, x.rating, x.type, x.season);
-        console.log(serieTV);
+        console.log(film.toString());
+    } else if (x.type === 'tv'){
+        const serieTV = new TvSerie(x.title, x.year, x.genre, x.rating, x.type, x.seasons);
+        console.log(serieTV.toString());
     }
 })
 
-//console.log(filterType)
+//Creiamo una funzione che restituisca la media dei voti di tutti i film per un determinato genere. 
+//Prevedere un argomento per la lista dei film ed uno per il genere.
+
+function mediaRating(genre = 'drama'){
+    let mediaFilm = 0;
+    for (const elem of arrayFilms) {
+        if(elem.genre === genre){
+            mediaFilm+= elem.rating
+        }
+    }
+    return `Il voto medio dei soli film di genere ${genre} è ${mediaFilm}`;
+}
+console.log(mediaRating('fantasy'))
+
+
 
